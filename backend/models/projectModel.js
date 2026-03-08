5d2d36125bae6ee091d932073e958efeb991ab1c
saikrishna-reddymalla <SAIKRISHNA.REDDYMALLA@GMAIL.COM>
2026-03-08 22:12:15 +0000

    Update projectModel.js

--- backend/models/projectModel.js
+++ backend/models/projectModel.js
@@ -1,1 +1,18 @@
-module.exports = {};
+const pool = require("../config/db");
+
+exports.getAllProjects = async () => {
+  const result = await pool.query(
+    "SELECT * FROM projects ORDER BY created_at DESC"
+  );
+  return result.rows;
+};
+
+exports.createProject = async (name, description, ownerId) => {
+  const result = await pool.query(
+    `INSERT INTO projects (name, description, owner_id)
+     VALUES ($1, $2, $3)
+     RETURNING *`,
+    [name, description, ownerId]
+  );
+  return result.rows[0];
+};
