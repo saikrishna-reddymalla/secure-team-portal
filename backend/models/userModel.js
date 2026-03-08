ccc0fcf419f4351573be9a292fd7fcb5e8d0e254
saikrishna-reddymalla <SAIKRISHNA.REDDYMALLA@GMAIL.COM>
2026-03-08 22:11:52 +0000

    Update userModel.js

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,1 +1,19 @@
-module.exports = {};
+const pool = require("../config/db");
+
+exports.findUserByEmail = async (email) => {
+  const result = await pool.query(
+    "SELECT * FROM users WHERE email = $1",
+    [email]
+  );
+  return result.rows[0];
+};
+
+exports.createUser = async (username, email, passwordHash) => {
+  const result = await pool.query(
+    `INSERT INTO users (username, email, password_hash)
+     VALUES ($1, $2, $3)
+     RETURNING id, username, email, role, created_at`,
+    [username, email, passwordHash]
+  );
+  return result.rows[0];
+};
